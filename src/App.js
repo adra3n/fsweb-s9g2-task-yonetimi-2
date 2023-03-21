55a8e62e14f21f7b8b218a810cc4608271720ff3
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-03-21 20:11:27 +0300

    Date added + Tailwind conversion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import "./app.css";
-import Task from "./Task";
-import TaskHookForm from "./TaskHookForm";
-import PeopleForm from "./PeopleForm";
-import { initialTasks, initialTeam } from "./data";
-import { toast } from 'react-toastify';
-
+import { useState } from 'react'
+import './app.css'
+import Task from './Task'
+import TaskHookForm from './TaskHookForm'
+import PeopleForm from './PeopleForm'
+import { initialTasks, initialTeam } from './data'
+import { toast } from 'react-toastify'
 
 function App() {
-  const [tasks, setTasks] = useState(initialTasks);
-  const [team, setTeam] = useState(initialTeam);
+  const [tasks, setTasks] = useState(initialTasks)
+  const [team, setTeam] = useState(initialTeam)
 
   function handleTaskSubmit(yeniTask) {
     setTasks([yeniTask, ...tasks])
@@ -20,17 +19,16 @@ function App() {
   }
 
   function handleComplete(id) {
-    const tasksCopy = [...tasks];
-    const ilgiliTask = tasksCopy.filter(t => t.id === id)[0];
-    ilgiliTask.status = "yapıldı";
-    setTasks(tasksCopy);
-
-    toast.success(`Tebrikler! "${ilgiliTask.title}" tamamlandı!`);
+    const tasksCopy = [...tasks]
+    const ilgiliTask = tasksCopy.filter((t) => t.id === id)[0]
+    ilgiliTask.status = 'yapıldı'
+    setTasks(tasksCopy)
+    toast.success(`Tebrikler! "${ilgiliTask.title}" tamamlandı!`)
   }
 
   return (
-    <div className="app">
-      <div className="formColumn">
+    <div className="h-screen flex">
+      <div className="bg-white flex-shrink-0 flex-none border-r-2 border-solid border-[#f3d4b0] overflow-auto">
         <div className="form-container">
           <h2>Yeni Task</h2>
           <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
@@ -41,31 +39,30 @@ function App() {
           <PeopleForm kisiler={team} submitFn={handlePeopleSubmit} />
         </div>
       </div>
-      <div className="columns">
-        <div className="column">
-          <h2 className="column-title">Yapılacaklar</h2>
+      <div className="flex-1 flex justify-center flex-wrap gap-8 pt-8 pr-8 pb-6 overflow-auto">
+        <div className="flex-1 min-w-[240px] max-w-[360px]">
+          <h2 className="text-2xl pl-3">Yapılacaklar</h2>
           <div className="column-list">
             {tasks
-              .filter((t) => t.status === "yapılacak")
+              .filter((t) => t.status === 'yapılacak')
               .map((t) => (
                 <Task key={t.id} taskObj={t} onComplete={handleComplete} />
               ))}
           </div>
         </div>
-        <div className="column">
-          <h2 className="column-title">Tamamlananlar</h2>
+        <div className="flex-1 min-w-[240px] max-w-[360px]">
+          <h2 className="text-2xl pl-3">Tamamlananlar</h2>
           <div className="column-list">
             {tasks
-              .filter((t) => t.status === "yapıldı")
+              .filter((t) => t.status === 'yapıldı')
               .map((t) => (
                 <Task key={t.id} taskObj={t} />
               ))}
           </div>
         </div>
       </div>
-
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Task.js
+++ src/Task.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React from 'react'
+import differenceInDays from 'date-fns/differenceInDays'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import tr from 'date-fns/locale/tr'
 
 const Task = ({ taskObj, onComplete }) => {
+  const deadlineDistance = formatDistanceToNow(new Date(taskObj.deadline), {
+    addSuffix: true,
+    locale: tr,
+  })
+  const dayDifference = differenceInDays(new Date(taskObj.deadline), new Date())
 
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
-      <p>{taskObj.description}</p>
-      <div>
+    <div className="p-6 bg-[#fff] rounded-md leading-relaxed mt-4 shadow-md">
+      <h3 className="text-xl text-[#c8781a]">{taskObj.title}</h3>
+      <div className="flex items-center justify-between text-sm font-medium text-[#444] mt-2">
+        <span>son teslim:</span>
+        <span
+          className={
+            dayDifference > 3
+              ? 'normal bg-[#d4d7ff] rounded px-2'
+              : 'urgent bg-[#ffd9d4] rounded px-2'
+          }
+        >
+          {deadlineDistance}
+        </span>
+      </div>
+      <p className="text-[#444] text-base leading-7 mt-2">
+        {taskObj.description}
+      </p>
+      <div className="mt-3">
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span
+            className="inline-block border border-solid border-[#ccc] rounded-full px-3 py-[5px] text-sm font-semibold text-[#444] mr-1 mb-1.5"
+            key={p}
+          >
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button
+          className="block mt-4 ml-auto bg-[#fecc91] px-4 py-2 rounded-full shadow-md"
+          onClick={() => onComplete(taskObj.id)}
+        >
+          Tamamlandı
+        </button>
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default Task;
+export default Task
